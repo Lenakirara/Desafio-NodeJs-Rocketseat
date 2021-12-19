0e7d4a692eb8c8e7bf702a3efa8d99c94920e8d3
Ghislaine <glenox79@gmail.com>
2021-12-19 12:15:52 -0300

    Adicionando validacao de token para criacao de resumos de estudo

--- src/routers/studySummaryRouter.js
+++ src/routers/studySummaryRouter.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 
 const {
+  validateJWTToken
+} = require('../middlewares/jwtTokenValidation');
+
+const {
   validateSummaries,
 } = require('../middlewares/studySummaryValidation');
 
@@ -14,8 +18,8 @@ const {
 
 router.get('/:id', findSummaryById);
 router.get('/', getAllSummaries);
-router.post('/', validateSummaries, createSummaries);
-router.put('/:id', updateSummary);
-router.delete('/:id', deleteSummary);
+router.post('/', validateJWTToken, validateSummaries, createSummaries);
+router.put('/:id', validateJWTToken, updateSummary);
+router.delete('/:id', validateJWTToken, deleteSummary);
 
 module.exports = router;
